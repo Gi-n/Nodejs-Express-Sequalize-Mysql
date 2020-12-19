17b5ce2e55f507e9dcb896fb75b1f61e33433008
Girish N Acharya <74775771+Gi-n@users.noreply.github.com>
2020-12-19 10:31:03 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,5 @@
 const userController = require('../controllers/userController');
 
 module.exports = function (app) {
-  // app.get('/api/user/all', user.getAllUsers);
-  // app.get('/api/user/get_users', user.getUsers);
-  // app.get('/api/user/:id', user.getUser);
-  // app.post('/api/user/status', multipartMiddleware, user.changeStatus);
   app.post('/api/v1/users/all', userController.getAllUsers);
-  // app.post('/api/user/resetPassword', user.changePassword);
-  // app.delete('/api/user/delete/:id', user.deleteUser);
 };
